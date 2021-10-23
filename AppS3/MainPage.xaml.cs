026b87409062f7da3b317e93d5ba86111008555c
EduLema <edulema1988@gmail.com>
2021-10-22 21:13:52 -0500

    Prueba

---
+++ AppS3/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+
+            // Almacenar los datos en variables 
+
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+
+
+            await Navigation.PushAsync(new viewDos(usuario,contrasena));
+
+        }
+    }
+}
---
+++ AppS3/viewDos.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class viewDos : ContentPage
+    {
+        public viewDos(string usuario, string contrasena)
+        {
+            InitializeComponent();
+            //Envio a los Label
+
+            lblUsuario.Tex = usuario;
+            lblContrasena,Tex = contrasena;
+
+
+
+
+
+
+        }
+    }
+}
